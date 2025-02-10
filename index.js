42c454376ff44820f3c522446ce8f1b926596961
Lucas Vladson C. de Lima <lucasvladsonhd@gmail.com>
2025-02-10 15:40:13 -0300

    up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(3000, (error) => {
+app.listen(port, (error) => {
     if(error) {
         console.log('Ocorreu um erro');
         return;
